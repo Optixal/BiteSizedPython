2de6f2fa493261ff8e2179e2bebe900aa3aedcf9
Optixal <optixal@live.com.sg>
2016-09-19 03:36:33 +0800

    Added Crypto AES: Encrypt & Decrypt ASCII text

---
+++ crypto_aes/decrypt_text.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+# Optixal
+
+# Encrypt ASCII plaintext
+# Using AES-CBC PKCS#7
+# with random 256-bit key
+# and random 128-bit (16-byte) IV
+
+import sys, codecs
+from Crypto.Cipher import AES
+from Crypto import Random
+
+template = "{0:20}: {1:}"
+
+def checkreq():
+    if len(sys.argv) != 3:
+        print("Usage:", sys.argv[0], "[ciphertext hex]", "[key hex]")
+        sys.exit(1)
+
+def unhexify(s):
+    return codecs.decode(s, 'hex')
+
+def main():
+    checkreq()
+    
+    ciphertext = unhexify(sys.argv[1])[16:]
+    key = unhexify(sys.argv[2])
+    iv = unhexify(sys.argv[1])[:16]
+    print(template.format("Ciphertext", sys.argv[1][32:]))
+    print(template.format("256-bit Key", sys.argv[2]))
+    print(template.format("128-bit IV", sys.argv[1][:32]))
+
+    # Decrypt
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext)
+    plaintext = plaintext[:-plaintext[-1]]
+    print(template.format("Ciphertext", plaintext.decode("UTF-8")))
+
+if __name__ == "__main__":
+    main()
---
+++ crypto_aes/encrypt_text.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+# Optixal
+
+# Encrypt ASCII plaintext
+# Using AES-CBC PKCS#7
+# with random 256-bit key
+# and random 128-bit (16-byte) IV
+
+import sys, codecs
+from Crypto.Cipher import AES
+from Crypto import Random
+
+template = "{0:20}: {1:}"
+
+def checkreq():
+    if len(sys.argv) != 2:
+        print("Usage:", sys.argv[0], "[plaintext]")
+        sys.exit(1)
+
+def hexify(s):
+    return codecs.encode(s, 'hex').decode("UTF-8").upper()
+
+def main():
+    checkreq()
+    print(template.format("Plaintext", sys.argv[1]))
+    
+    # Generate Random 256-bit Key and 128-bit IV
+    key = Random.new().read(32)
+    iv = Random.new().read(16)
+    print(template.format("256-bit Key", hexify(key)))
+    print(template.format("128-bit IV", hexify(iv)))
+
+    # Convert and Pad Plaintext
+    plaintext = sys.argv[1].encode()
+    pad_len = 16 - (len(plaintext) % 16)
+    plaintext += bytes([pad_len]) * pad_len
+
+    # Encrypt
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = iv + cipher.encrypt(plaintext)
+    print(template.format("Ciphertext", hexify(ciphertext)))
+
+if __name__ == "__main__":
+    main()
